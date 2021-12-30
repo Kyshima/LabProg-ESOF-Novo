3170663943253272c936c11bb88a6205805190e7
rpcmoreira <rpcardoso2@outlook.com>
2021-12-30 14:55:13 +0000

    pagina inicial

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,5 +5,7 @@ Home
 @endsection
 
 @section('content')
-<h2 class="mt-5 mb-3 text-center">Laravel Programming Lab.!</h2>
+<h1 class="mt-5 mb-3 text-center text-primary">Eterno Candidato</h1>
+<h4 class="mt-5 mb-3 text-center">És um empregador à procura do teu próximo talento? Ou uma pessoa à procura de um novo desafio para a tua carreira profissional?</h4>
+<h4 class="mt-5 mb-3 text-center">Poderás encontrar tudo isso e mais, aqui só no Eterno Candidato!</h4>
 @endsection
