d9fcde929e5cbd65391972f33c25fe5f7e634ade
rpcmoreira <rpcardoso2@outlook.com>
2022-01-01 18:05:05 +0000

    Update informacao do User/Empresa, Novo welcome e mensagens, update password

--- resources/views/empresa/edit.blade.php
+++ resources/views/empresa/edit.blade.php
@@ -8,14 +8,14 @@
                 <div class="card-header">{{ __('Edit') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ url('/update'.$user) }}">
+                    <form method="POST" action="{{ url('/update') }}">
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Company Name') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ $user->name }}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -26,12 +26,22 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="position" class="col-md-4 col-form-label text-md-end">{{ __('Position Wanted') }}</label>
+                            <label for="position_main" class="col-md-4 col-form-label text-md-end">{{ __('Position Area') }}</label>
 
                             <div class="col-md-6">
-                                <input id="position" type="text" class="form-control @error('position') is-invalid @enderror" name="position" value="{{ old('position') }}" required autocomplete="position" autofocus>
+                                <!--<input id="position_main" type="text" class="form-control @error('position') is-invalid @enderror" name="position_main" value="{{ old('position_main') }}" required autocomplete="position_main" autofocus>-->
+                                <select id="position_main" name="position_main" class="form-select form-control @error('position_main') is-invalid @enderror" autofocus>
+                                    <option {{($user->position_main) == 'Administrative' ? 'selected' : ''}}  value="Administrative">Administrative</option>
+                                    <option {{($user->position_main) == 'Computer Science' ? 'selected' : ''}}  value="Computer Science">Computer Science</option>
+                                    <option {{($user->position_main) == 'Culinary' ? 'selected' : ''}}  value="Culinary">Culinary</option>
+                                    <option {{($user->position_main) == 'Design' ? 'selected' : ''}}  value="Design">Design</option>
+                                    <option {{($user->position_main) == 'Education' ? 'selected' : ''}}  value="Education">Education</option>
+                                    <option {{($user->position_main) == 'Public Services' ? 'selected' : ''}}  value="Public Services">Public Services</option>
+                                    <option {{($user->position_main) == 'Services to the Public' ? 'selected' : ''}}  value="Services to the Public">Services to the Public</option>
+                                    <option {{($user->position_main) == 'Other' ? 'selected' : ''}}  value="Other">Other</option>
+                                </select>
 
-                                @error('position')
+                                @error('position_main')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
@@ -40,12 +50,60 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="localization" class="col-md-4 col-form-label text-md-end">{{ __('Localization') }}</label>
+                            <label for="position_sec" class="col-md-4 col-form-label text-md-end">{{ __('Position') }}</label>
 
                             <div class="col-md-6">
-                                <input id="localization" type="text" class="form-control @error('localization') is-invalid @enderror" name="localization" value="{{ old('localization') }}" required autocomplete="localization" autofocus>
+                                <input id="position_sec" type="text" class="form-control @error('position_sec') is-invalid @enderror" name="position_sec" value="{{ $user->position_sec }}" required autocomplete="position_sec" autofocus>
 
-                                @error('localization')
+                                @error('position_sec')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="localization_main" class="col-md-4 col-form-label text-md-end">{{ __('Localization City') }}</label>
+
+                            <div class="col-md-6">
+                                <!--<input id="localization_main" type="text" class="form-control @error('localization_main') is-invalid @enderror" name="localization_main" value="{{ old('localization_main') }}" required autocomplete="localization_main" autofocus>-->
+                                <select id="localization_main" name="localization_main" class="form-select form-control @error('localization_main') is-invalid @enderror" autofocus>
+                                    <option {{($user->localization_main) == 'Aveiro' ? 'selected' : ''}} value="Aveiro">Aveiro</option>
+                                    <option {{($user->localization_main) == 'Beja' ? 'selected' : ''}} value="Beja">Beja</option>
+                                    <option {{($user->localization_main) == 'Braga' ? 'selected' : ''}} value="Braga">Braga</option>
+                                    <option {{($user->localization_main) == 'Bragança' ? 'selected' : ''}} value="Bragança">Bragança</option>
+                                    <option {{($user->localization_main) == 'Castelo Branco' ? 'selected' : ''}} value="Castelo Branco">Castelo Branco</option>
+                                    <option {{($user->localization_main) == 'Coimbra' ? 'selected' : ''}} value="Coimbra">Coimbra</option>
+                                    <option {{($user->localization_main) == 'Faro' ? 'selected' : ''}} value="Faro">Faro</option>
+                                    <option {{($user->localization_main) == 'Guarda' ? 'selected' : ''}} value="Guarda">Guarda</option>
+                                    <option {{($user->localization_main) == 'Leiria' ? 'selected' : ''}} value="Leiria">Leiria</option>
+                                    <option {{($user->localization_main) == 'Lisboa' ? 'selected' : ''}} value="Lisboa">Lisboa</option>
+                                    <option {{($user->localization_main) == 'Portalegre' ? 'selected' : ''}} value="Portalegre">Portalegre</option>
+                                    <option {{($user->localization_main) == 'Porto' ? 'selected' : ''}} value="Porto">Porto</option>
+                                    <option {{($user->localization_main) == 'Santarém' ? 'selected' : ''}} value="Santarém">Santarém</option>
+                                    <option {{($user->localization_main) == 'Setubal' ? 'selected' : ''}} value="Setubal">Setubal</option>
+                                    <option {{($user->localization_main) == 'Viana do Castelo' ? 'selected' : ''}} value="Viana do Castelo">Viana do Castelo</option>
+                                    <option {{($user->localization_main) == 'Vila Real' ? 'selected' : ''}} value="Vila Real">Vila Real</option>
+                                    <option {{($user->localization_main) == 'Viseu' ? 'selected' : ''}} value="Viseu">Viseu</option>
+                                    <option {{($user->localization_main) == 'Évora' ? 'selected' : ''}} value="Évora">Évora</option>
+                                </select>
+
+                                @error('localization_main')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="localization_sec" class="col-md-4 col-form-label text-md-end">{{ __('Localization') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="localization_sec" type="text" class="form-control @error('localization') is-invalid @enderror" name="localization_sec" value="{{ $user->localization_sec }}" required autocomplete="localization_sec" autofocus>
+
+                                @error('localization_sec')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
@@ -57,7 +115,7 @@
                             <label for="years" class="col-md-4 col-form-label text-md-end">{{ __('Years of Experience') }}</label>
 
                             <div class="col-md-6">
-                                <input id="years" type="number" class="form-control @error('years') is-invalid @enderror" name="years" value="{{ old('years') }}" required autocomplete="years">
+                                <input id="years" type="number" class="form-control @error('years') is-invalid @enderror" name="years" value="{{ $user->years }}" required autocomplete="years">
 
                                 @error('years')
                                     <span class="invalid-feedback" role="alert">
@@ -71,7 +129,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $user->email }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -81,37 +139,10 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <?php
-                        $n=NULL;
-                        echo "
-                        <input type='hidden' name='type' value='0'>
-                        <input type='hidden' name='lastName' value=$n>
-                        <input type='hidden' name='default' value=$n>
-                        <input type='hidden' name='img' value=$n>
-                        ";
-                        ?>
+                        <input type='hidden' name='img' value='{{ $user->img }}'>
+                        <input type='hidden' name='default' value='{{ $user->default }}'>
+                        <input type='hidden' name='type' value='{{ $user->type }}'>
+                        <input type='hidden' name='lastName' value='{{ $user->lastName }}'>
                         
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
